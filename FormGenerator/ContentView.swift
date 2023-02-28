a5307d11fbb27c949f47c782861fbd3cf2eba38a
DuTomi <dudas.tamas.alex@gmail.com>
2023-02-28 19:11:44 +0100

    Initial Commit

---
+++ FormGenerator/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FormGenerator
+//
+//  Created by Dudas Tamas Alex on 2023. 02. 28..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+// https://github.com/DuT0mi/FormGenerator-Onlab.git
+
---
+++ FormGenerator/FormGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormGeneratorApp.swift
+//  FormGenerator
+//
+//  Created by Dudas Tamas Alex on 2023. 02. 28..
+//
+
+import SwiftUI
+
+@main
+struct FormGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
