ec44b8498651015a1f897eedf0d5213d16be9a43
DuTomi <dudas.tamas.alex@gmail.com>
2023-03-04 17:22:59 +0100

    FireBase set up.

---
+++ FormGenerator/UtilityExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  UtilityExtensions.swift
+//  FormGenerator
+//
+//  Created by Dudas Tamas Alex on 2023. 02. 28..
+//
+
+import Foundation
--- FormGenerator/Views/FormGeneratorView.swift
+++ FormGenerator/Views/FormGeneratorView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FormGeneratorView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,9 +19,9 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FormGeneratorView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FormGeneratorView()
     }
 }
 	
