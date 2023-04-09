4055bbcd01f23f20f0316caf92d91c46508ad840
DuTomi <dudas.tamas.alex@gmail.com>
2023-04-09 18:34:50 +0200

    Uploading, downloading and showing them after all.

--- FormGenerator/Models/ViewModels/Core/FormListViewModel.swift
+++ FormGenerator/Models/ViewModels/Core/FormListViewModel.swift
@@ -2,18 +2,25 @@ import Foundation
 
 @MainActor
 final class FormListViewModel: ObservableObject {
+    @Published private(set) var forms: [FormData] = []
     @Published private(set) var account: Account?
     @Published private(set) var isAccountLoaded: Bool
+    
+    private let authManager: AuthenticationManager = AuthenticationManager()
     
     init(){
         _isAccountLoaded = .init(wrappedValue: false)
     }
     
-    private let authManager: AuthenticationManager = AuthenticationManager()
-    
     func loadCurrentAccount() async throws {
         let authDataResult = try authManager.getAuthenticatedUser()
         self.account = try await AccountManager.shared.getUserByJustID(userID: authDataResult.uid)
+        // Must to adjust it true, and not toggle because of the User can tap back from creating the View and then will disappear the oppurtinity to create
         self.isAccountLoaded = true
+    }
+    func downloadAllForm(){
+        Task{
+            self.forms = try await FormManager.shared.downloadAllForm()
+        }
     }
 }
---
+++ FormGenerator/Models/ViewModels/Core/FormManager.swift
@@ -0,0 +1,22 @@
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+final class FormManager{
+    // MARK: Singleton
+    static let shared: FormManager = FormManager()
+    private init() { }
+    
+    private let formCollection = Firestore.firestore().collection("forms")
+    private func formDocument(formID: String) -> DocumentReference {
+        formCollection.document(formID)
+    }
+    
+    
+    func uploadFormToDatabase(form: FormData) async throws {
+        try formDocument(formID: String(form.id)).setData(from: form, merge:false)
+    }
+    func downloadAllForm() async throws -> [FormData]{
+        try await formCollection.getDocuments(as: FormData.self)
+    }
+}
